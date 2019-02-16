1d3cc8888e3ef0358de0a15e5dd5be8f8d9d4a5c
Yun Jay Kim <yunjay@users.noreply.github.com>
2019-02-16 14:19:29 +0900

    55
    555

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,12 @@
 const Mutations = {
     //args = 입력
-    createDog(parent,args,ctx,info){
-    global.dogs = global.dogs || [];
-    //or empty array
-    const newDog = {name: args.name};
-    console.log(args);
-    global.dogs.push(newDog);
-    return newDog;
+    createItem(parent,args,ctx,info){
+
+        const item = ctx.db.mutation.createItem({
+            data:{
+                ...args //input data. (title: args.title etc.)
+            } 
+        }, info) //context.database
     }
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,7 @@
 // 4 vars : parent, args(arguments) , ctx(context of request ex) cookies) ,info 
 const Query = {
-    dogs(parent,args,ctx,info){
-        global.dogs = global.dogs || [];
-        return[{ name:'누렁'},{name:'똥똥'}];
-    },
+    //(parent,args,ctx,info){
+    //},
 };
 
 module.exports = Query;
